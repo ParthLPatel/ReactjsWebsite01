0c49da16a357a1b0514de8e26b07317f8aa26691
parth <parthlpatel76@gmail.com>
2020-11-25 12:12:35 +0530

    v1.0 c1 front-end using reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import React from 'react';
+//import bootstrap:
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import HomePage from './Home';
+import AboutPage from './About';
+import ContactPage from './Contact';
+import ServicePage from './Service';
+
+const App = ()=>{
+  return(
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/contact" component={ContactPage} />
+        <Route exact path="/service" component={ServicePage} />
+        <Redirect to="/"/>
+      </Switch>
+      <br />
+      <br />
+      <br />
+      <br />
+
+  {/*<Footer />*/}
+    </React.Fragment>
   );
 }
 
-export default App;
+export default App; 
